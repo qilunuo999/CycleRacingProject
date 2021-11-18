990bfef94698c1ab1adb51daf1ea89eff0e0d394
qilunuo999 <1176234504@qq.com>
2021-11-18 14:50:47 +0800

    赛车项目创建

---
+++ Source/CycleRacingProject/CycleRacingProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CycleRacingProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CYCLERACINGPROJECT_API ACycleRacingProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
